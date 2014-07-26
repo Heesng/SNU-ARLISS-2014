8e58a01e986607c0c5139428f856c4297b5b85a5
kimnerd <hyuntaec@naver.com>
2014-07-26 14:30:29 +0900

    차 통신 코드 추가

--- ARLISS TEST/Car/Car.ino
+++ ARLISS TEST/Car/Car.ino
@@ -3,30 +3,47 @@
 #include <LSM303.h>
 #include <Math.h>
 #include <Servo.h>
+#include <RF.h>
+#include <SD.h>
+#include <DCS.h>
 
 /*
 Car functions
 */
 void vel(int a);
 void steer(float a,float b,float c,float d,float e);
-void go(float a,float b,float c,float d,float e, float f);
+void go(float a,float b,float c,float d,float e, float f, float g);
+void sdWrite(String dataString);
+float distance(GPS gps_, DCS dcs_);
 
 /*
 Car Variables
 */
-GPS gpsEx;
+DCS dcs;
+GPS gps;
+RF rf;
 Servo Carsteer;
 Servo Carspeed;
 LSM303 compass;
+
 float destlat = 37.275371;
 float destlong = 126.569137;
 float steer_car,dsteer_car,isteer_car = 0, steer_car0;
 float Pgain = 3, Dgain, Igain;
-float asteer_car,adsteer_car,aisteer_car = 0, asteer_car0;
-float aPgain = 3, aDgain, aIgain;
+const String module = "R";
+const int chipSelect = 9; //SD card chip select
+char x [6];
+
+int d,g=0, r=0; //dcs,gps,rfrcv
+int D=0; //, Docking
+float dist=1000;
+String state = "";
+String rcvPck = "";
 
 void setup(){
+  state = "S";
   Serial.begin(9600);
+  Serial1.begin(1200);
   Serial2.begin(9600);
   //for compass sensor
   Wire.begin();
@@ -46,18 +63,45 @@ void setup(){
 }
 
 void loop(){
-  gpsEx.renewGPS();
+  g = gps.renew();
   //Serial.println("a");
-  float lat = gpsEx.getLat()/100;
-  float lng = gpsEx.getLng()/100;
+  float lat = gps.getLat()/100;
+  float lng = gps.getLng()/100;
   //double satheading = 0;//rf.getheading();
   //for compass sensor
   //Serial.println("b");
   compass.read();
   //Serial.println("c");
   float heading_ = compass.heading();
+  dtostrf(heading_,3,1,x);
   //Serial.println("d");
-  go(destlat,destlong,lat,lng, heading_, sheading_);
+  
+  if(g==1){
+  dcs.mergeData(module,state,gps.getSLat(),gps.getSLng(),gps.getSHgt(),x);
+  //sdWrite(dcs.getSdData());
+  rf.sendPck(dcs.getRfData());
+  }
+  r = rf.receivePck(rcvPck);
+  if(r==0){
+  dcs.readPck(rcvPck);
+  dist = distance(gps,dcs);
+  }
+  
+  /*
+use dcs functions dcs.function();
+
+ 	String getSLat();
+	String getSLng();
+	String getSHgt();
+	String getSHeading();
+	double getLat();
+	double getLng();
+	double getHgt();
+	double getHeading();
+
+*/
+
+  go(dcs.getLat(),dcs.getLng(),lat,lng, heading_, dcs.getHeading());
   //Serial.println(lat,6);
   //Serial.println(lng,6);
 //  steer(destlat,destlong,lat,lng,heading_);
@@ -65,16 +109,22 @@ void loop(){
 }
 
 void go(float destlat,float destlong,float lat,float lng,float heading_, float sheading_){
-  float semidestlat = destlat - 0.0009*cos(sheading_);
-  float semidestlong = destlong - 0.0009*sin(sheading_);
-    if((destlat-lat)*(destlat-lat) + (destlong-lng)*(destlong-lng)> 0.0009*0.0009){
-      vel(60);
-      steer(semidestlat,semidestlong,lat,lng,heading_);
-    }
-    else{
-      vel(30);
-      steer(destlat,destlong,lat,heading_);
-    }
+  float semidestlat = destlat - 0.0009*sin(sheading_);
+  float semidestlong = destlong - 0.0009*cos(sheading_);
+  
+  if(((destlat - lat)*(destlat - lat) + (destlong - lng)*(destlong - lng)) < ((semidestlat - lat)*(semidestlat - lat) + (semidestlong - lng)*(semidestlong - lng))){
+    semidestlat = destlat + 0.0009*sin(sheading_);
+    semidestlong = destlong + 0.0009*cos(sheading_);
+  }
+  
+  if((destlat-lat)*(destlat-lat) + (destlong-lng)*(destlong-lng)> 0.0009*0.0009){
+    vel(60);
+    steer(semidestlat,semidestlong,lat,lng,heading_);
+  }
+  else{
+    vel(30);
+    steer(destlat,destlong,lat,lng, heading_);
+  }
 }
 
 void steer(float destlat,float destlong,float flatitude,float flongitude, float heading_){
@@ -113,7 +163,6 @@ void steer(float destlat,float destlong,float flatitude,float flongitude, float 
     psteer_car = psteer_car - 360;
   }
   
-  
   dsteer_car = psteer_car - steer_car0;
   steer_car0 = psteer_car;
   isteer_car = isteer_car + dsteer_car;
@@ -138,25 +187,11 @@ void steer(float destlat,float destlong,float flatitude,float flongitude, float 
   else if(90.0 <= psteer_car && psteer_car < 180.0){ 
     osteer = 60;
   }
-  
-//  if (0 <= steer_car && steer_car < 180){
-//    if(osteer>180){
-//      osteer = 180;
-//    }
-//    osteer = map(osteer, 0 , 180 , 90 , 0);
-//  }
-//  else if(-180 <= steer_car && steer_car < 0){
-//    if(osteer<-180){
-//      osteer = -180;
-//    }
-//    osteer = map(osteer, -180 , 0 , 180 , 90);
-//  }
   Carsteer.write(osteer);
   Serial.println(osteer);
 }
 
 void vel(int velocity){
-//  for(int i = 0; i<10;i++){
   unsigned long settime = millis();
   unsigned long currenttime = millis();
   unsigned long ftime = settime + velocity;
@@ -179,6 +214,36 @@ void vel(int velocity){
     Carspeed.write(90);
     currenttime = millis();
   }
-//  }
 }
 
+
+
+
+void sdWrite(String dataString){
+	File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+ 	 // if the file is available, write to it:
+ 	 if(dataFile){
+ 	 	dataFile.println(dataString);
+ 	 	dataFile.close();
+
+    	// print to the serial port too:
+    	Serial.println(dataString);
+    }
+}
+
+float distance(GPS gps_, DCS dcs_){
+	float lat1 = gps_.getLat();
+	float lng1 = gps_.getLng();
+
+	float lat2 = dcs_.getLat();
+	float lng2 = dcs_.getLng();
+
+	float latL, lngL, dist;
+	latL = 2*3.14159*6378000*(lat1-lat2)/100/180;
+	lngL = 2*3.14159*6378000*(lat1-lat2)/100/180;
+
+	dist = sqrt(latL*latL+lngL*lngL);
+
+	return dist;
+}
