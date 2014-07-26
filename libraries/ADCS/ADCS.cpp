6c9fae6e0d0ae39925592c085c7e7699b2167246
Heesng <heesng57@gmail.com>
2014-07-26 10:11:48 +0900

    ADCS lib renew
    - reeling parachute function added

--- libraries/ADCS/ADCS.cpp
+++ libraries/ADCS/ADCS.cpp
@@ -1,42 +1,124 @@
+#include "Arduino.h"
+#include "ADCS.h"
 
 ADCS::ADCS(){
-	 pinMode(enablePin, OUTPUT);
-	 pinMode(dirPin, OUTPUT);
-	 pinMode(pwmPin, OUTPUT);
+	pwmPin1 = 2;
+	enablePin1 = 4;
+	dirPin1 =3;
+
+	pwmPin2 = 5;
+	enablePin2 = 7;
+	dirPin2 = 6;
+
+	XAccPin = 0;
+	YAccPin = 1;
+	ZAccPin = 2;
+	
+	Acc[3];
+	theta = 0;
+	dtheta = 0;
+	theta0 =0;
+
+	PGain=6;
+	DGain=0;
+	
+	pinMode(enablePin1, OUTPUT);
+	pinMode(dirPin1, OUTPUT);
+	pinMode(pwmPin1, OUTPUT);
+	pinMode(enablePin2, OUTPUT);
+	pinMode(dirPin2, OUTPUT);
+	pinMode(pwmPin2, OUTPUT);
 }
 
-
-void ADCS::control(){
+int ADCS::renew(){
 	//Attitude Determination
  	//Read Sensor Data
- 	for(int i=0; i<4; i++){
+	for(int i=0; i<4; i++){
 		Acc[i] = analogRead(i)-333;
 	}
 	//Calculate 1-D Attitude
 	//Reference input for object attitude is theta = 0
-	theta = (180/3.14)*atan2(Acc[1], Acc[2])-0.5;
+	theta = (180/3.14)*atan2(Acc[2], Acc[1])-0.5;
+	dtheta = theta - theta0;
+	theta0 = theta;
+	if(theta < 95 && theta > 75) return 0;	//when sensor is erect
+	else return 1;
+}
+
+
+void ADCS::control(){
+	int i = 1;
+
+	while(i==1){
+
+	//Attitude Control
+	//Object attitude of 
+	//Enable Motor
+		digitalWrite(enablePin2, LOW);
+		
+		if(theta>140||theta<-140){
+			theta=140;
+		}
+		if(theta<84){
+			digitalWrite(dirPin2, LOW);
+			wr =-1*PGain*((theta-84)+ DGain*dtheta);
+		}
+		else {
+			digitalWrite(dirPin2, HIGH);
+			wr = (PGain*((theta-84)+ DGain*dtheta));
+		}
+		wrInt = (int) wr;
+		if(wrInt>255){
+			wrInt=255;
+		}
+		analogWrite(pwmPin2, wrInt);
+		i = renew();
+	}
+	digitalWrite(enablePin2, HIGH);
+
+}
+
+int ADCS::reelPara(){
+	//Attitude Determination
+ 	//Read Sensor Data
+	for(int i=0; i<4; i++){
+		Acc[i] = analogRead(i)-333;
+	}
+	//Calculate 1-D Attitude
+	//Reference input for object attitude is theta = 0
+	theta = (180/3.14)*atan2(Acc[2], Acc[1])-0.5;
 	dtheta = theta - theta0;
 	theta0 = theta;
 	//Attitude Control
 	//Object attitude of 
 	//Enable Motor
-	digitalWrite(enablePin, LOW);
-	if(theta>0){
-	  digitalWrite(dirPin, HIGH);
-	  wr = PGain*theta+ DGain*dtheta;
+
+	digitalWrite(enablePin1, LOW);
+	
+	if(theta>140||theta<-140){
+		theta=140;
+	}
+	if(theta<84){
+		digitalWrite(dirPin1, HIGH);
 	}
 	else {
-	   digitalWrite(dirPin, LOW);
-	   wr = -1*(PGain*theta+ DGain*dtheta);
+		digitalWrite(dirPin1, LOW);
 	}
 
-	analogWrite(pwmPin, wr);
-}
+	/*
+	for(int i =0; i<255; i++){
+		analogWrite(pwmPin1, i);
+		delay(40);
+	}*/
 
 
-int ADCS::checkAtt(){
-	if(25 < theta && theta < -25)
-	{ return 0;}
-	else
-		return -1;
-}
+		analogWrite(pwmPin1, 250);
+		delay(8000);
+		digitalWrite(enablePin1, HIGH);
+
+		return 1;
+	}
+
+
+
+	float ADCS::gettheta(){return theta;}
---
+++ libraries/ADCS/ADCS/ADCS.ino
@@ -0,0 +1,20 @@
+#include <ADCS.h>
+
+ADCS adcs;
+
+float a;
+int i = 0, j = 0;
+
+void setup(){
+	Serial.begin(9600);
+}
+
+void loop(){
+	if(i==0){
+		i = adcs.reelPara();
+	}
+	j = adcs.renew();
+	if(j==1){
+		adcs.control();
+	}
+}
