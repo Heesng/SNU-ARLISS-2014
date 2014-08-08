9bd6830ad0850cdcd64801eff22a5ce7aeabfb8c
kimnerd <hyuntaec@naver.com>
2014-08-08 12:06:42 +0900

    quark esc 연습

---
+++ ArduinoCodes/quarktest/quarktest.ino
@@ -0,0 +1,30 @@
+int motor = 2;           // the pin that the LED is attached to
+
+// the setup routine runs once when you press reset:
+void setup()  { 
+//  int myEraser = 7;             // this is 111 in binary and is used as an eraser
+//  TCCR3B &= ~myEraser;   // this operation (AND plus NOT),  set the three bits in TCCR2B to 0
+//  int myPrescaler = 2;         // this could be a   number in [1 , 6]. In this case, 3 corresponds in binary to 011.   
+//  TCCR3B |= myPrescaler;  //this operation (OR), replaces the last three bits in TCCR2B with our new value 011
+//  // declare pin 9 to be an output:
+  delay(10000);
+//  analogWrite(motor, 210);
+//  delay(10);
+  for(int i = 0; i <= 255 ; i++){
+    analogWrite(motor, i);
+    delay(10);
+  }
+  analogWrite(motor, 167);
+  delay(1000);
+  analogWrite(motor, 157);
+  delay(1000);
+  
+} 
+
+// the loop routine runs over and over again forever:
+void loop()  { 
+    analogWrite(motor, 160);
+    delay(1000);
+    //delay(10);               
+}
+
